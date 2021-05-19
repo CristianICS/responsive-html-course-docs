73baa53bd15b08576c4228730203e9fa384c4bdd
Cristian Iranzo <58115393+CristianICS@users.noreply.github.com>
2021-05-19 20:45:14 +0200

    Add script js

---
+++ script/script.js
@@ -0,0 +1,30 @@
+/* Toggle between adding and removing the "responsive" class to navbar when the user clicks on the icon */
+function myFunction() {
+  var x = document.getElementById("table-of-contents");
+  if (x.className === "TOC") {
+    x.className += " responsive";
+  } else {
+    x.className = "TOC";
+  }
+}
+
+
+/* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger icon */
+function TOC_function() {
+	var x = document.getElementById("table-of-contents");
+	
+	if (x.style.display === "block") {
+    x.style.display = "none";
+	document.getElementById("canvas").style.left = "0vw";
+	document.getElementById("canvas").style.width = "100vw";
+	document.getElementById("navbar-menu").style.width = "100vw";
+	document.getElementById("left-arrow").style.left = "0vw";
+  } else {
+    x.style.display = "block";
+	document.getElementById("canvas").style.left = "15vw";
+	document.getElementById("canvas").style.width = "85vw";
+	document.getElementById("navbar-menu").style.width = "85vw";
+	document.getElementById("left-arrow").style.left = "15vw";
+  }
+}
+
